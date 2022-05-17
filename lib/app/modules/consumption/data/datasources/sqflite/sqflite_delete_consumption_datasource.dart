dc63d7c4d0918a37ef0741732bae1370a7708db8
Diego Ventura <diegoventurasg@gmail.com>
2022-05-17 09:22:26 -0300

    remove unused imports

--- lib/app/modules/consumption/data/datasources/sqflite/sqflite_delete_consumption_datasource.dart
+++ lib/app/modules/consumption/data/datasources/sqflite/sqflite_delete_consumption_datasource.dart
@@ -1,6 +1,4 @@
 import '../../../../../core/data/storages/sqflite/repositories/consumption_repository.dart';
-import '../../../domain/entities/consumption_entity.dart';
-import '../../dtos/consumption_dto.dart';
 import '../delete_consumption_datasource.dart';
 
 class SqfliteDeleteConsumptionDatasource
